7e39c66a25bba5f0dc76487972b44d43823d7229
Warren Smith <warren@wandrsmith.net>
2013-11-09 01:34:59 -0600

    Fix problem in setup.py. Add release_notes.

---
+++ docs/release_notes.rst
@@ -0,0 +1,8 @@
+
+0.0.2
+=====
+ * Fix problem in setup.py.
+
+0.0.1
+=====
+ * First release.
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 from setuptools import setup, find_packages
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 def file_read(filename):
     filepath = os.path.join(os.path.dirname(__file__), filename)
